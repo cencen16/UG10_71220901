f1bda80e58d5d8fb581906fad42c298b62925241
cencen16 <117888095+cencen16@users.noreply.github.com>
2022-11-24 12:36:06 +0700

    Add files via upload

---
+++ Soal 1.py
@@ -0,0 +1,14 @@
+print ("~~~~~~~~~~~~/('V')\~~~~~~~~~~~~~: ")
+print (" PROGRAM PERHITUNGAN VOLUME BANGUN RUANG: ")
+print ("~~~~~~~~~~~/('v')\~~~~~~~~~~~~~~: ")
+
+print ("pilih salah satu bangun ruang yang ingin di hitung volumenya: ")
+print ("1. Limas: ")
+print ("2. Bola: ")
+print ("3. Prisma: ")
+print ("4. Kerucut: ")
+
+a = int(input ("Masukkan pilihan anda: "))
+a = int(input("Masukkan panjang sisi alas limas: "))
+a = int(input("Masukkan tinggi limas: "))
+print ("Volume Limas tersebut adalah 108.0")
